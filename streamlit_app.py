c97cf55815a565126b021f6560ba52d4f6f14bd5
ChangHwanLee <lch3491@gmail.com>
2024-08-21 07:28:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,5 @@ with st.expander('Data'):
 with st.expander('Data Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   
-  
+with st.sidebar:
+  st.header('Input features')
